96e7d336fc7eee0601b50b7c9447a73b2031d810
Emmanuel Odekunle <nuelodekunle20@gmail.com>
2026-02-27 04:53:48 +0100

    yhb

--- hmeaffilate/upload-house.php
+++ hmeaffilate/upload-house.php
@@ -1,5 +1,10 @@
 <?php 
 // ob_start();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+
 session_start(); // <- Add this line to start the session
 include ("../inc/session.php"); 
 include ("../inc/connect.inc.php");
