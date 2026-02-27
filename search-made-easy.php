6f77fbfa97cb779b2c1866ddba2556bdbeb4e1d2
Emmanuel Odekunle <nuelodekunle20@gmail.com>
2026-02-27 10:13:08 +0100

    hdhbdhdbshdb

--- search-made-easy.php
+++ search-made-easy.php
@@ -1,4 +1,10 @@
 <?php
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+
 include ('inc/session.php');
 include ("inc/connect.inc.php");
  if(isset($_GET['code'])){
