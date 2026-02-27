6aa8d7e526d8d04ca74d01113bfc81588ec8e8ab
Emmanuel Odekunle <nuelodekunle20@gmail.com>
2026-02-27 09:38:40 +0100

    ddndndn

--- index.php
+++ index.php
@@ -795,7 +795,7 @@ if (isset($_COOKIE['user_id']) && isset($_COOKIE['user_email'])) {
     <!-- Navigation -->
     <nav class="navbar" id="navbar">
         <div class="nav-container">
-            <div class="logo">🏠 House Made Easy</div>
+            <div class="logo">                <img src="assets/images/HouseMadeEasylogo.jpg" alt="Modern Apartment"></div>
             <ul class="nav-menu">
                 <li><a href="#home" class="nav-link">Home</a></li>
                 <li><a href="#services" class="nav-link">Services</a></li>
